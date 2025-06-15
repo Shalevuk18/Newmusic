42fb6282c3a6e393d3e29260803c0fa1bfeb6c1b
Shalevuk18 <shalevuk@gmail.com>
2025-06-15 09:26:10 +0300

    finel finel version

--- js/app.js
+++ js/app.js
@@ -5,5 +5,5 @@ function playTrack(src) {
   player.play();
 }
 function toggleFrequency() {
-  alert("Simulated switch to 432Hz/528Hz (actual processing requires backend engine).");
+  alert("Frequency toggled (432Hz / 528Hz simulation)");
 }
